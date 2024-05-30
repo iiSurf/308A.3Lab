f2d69f5fc92ee49b2752cad260f2b7c07bd7afb0
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-30 08:13:20 -0700

    add try catch

--- index.mjs
+++ index.mjs
@@ -2,17 +2,43 @@
 import { central, db1, db2, db3, vault } from "./databases.js";
 
 async function getUserInfo(id) {
-  const userInformation = await central(id);
+  try {
 
-  const basicInformation1 = await db1(Id);
-  const basicInformation2 = await db2(Id);
-  const basicInformation3 = await db3(Id);
+    const dbName = await central(id);
 
-  const personalData = await vault(id);
+    const userData1 = await db1(id);
+    const userData2 = await db2(id);
+    const userData3 = await db3(id);
+
+    const personalData = await vault(id);
+
+    const userObject = {
+      id, // or is it id: id?
+      name: personalData.name,
+      username: userData.username,
+      email: personalData.email,
+      address: {
+        street: personalData.address.street,
+        suite: personalData.address.suite,
+        city: personalData.address.city,
+        zipcode: personalData.address.zipcode,
+        geo: {
+          lat: personalData.address.geo.lat,
+          lng: personalData.address.geo.lng,
+        },
+      },
+      phone: personalData.phone,
+      website: userData.website,
+      company: {
+        name: userData.company.name,
+        catchPhrase: userData.company.catchPhrase,
+        bs: userData.company.bs,
+      },
+    };
+
+    return userObject;
+  } catch (error) {
+
+    throw new Error(`Can not get information: ${error.message}`);
+  }
 }
-
-const dbs = {
-  db1: db1,
-  db2: db2,
-  db3: db3
-};
