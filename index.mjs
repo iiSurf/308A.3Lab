0a5ac69fed289eb556476b15745e39605758bfae
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-30 06:32:46 -0700

    basic information updated

--- index.mjs
+++ index.mjs
@@ -1,7 +1,15 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
+async function getUserInfo(id) {
+  const userInformation = await central(id);
 
+  const basicInformation1 = await db1(Id);
+  const basicInformation2 = await db2(Id);
+  const basicInformation3 = await db3(Id);
+
+  const personalData = await vault(id);
+}
 
 const dbs = {
   db1: db1,
